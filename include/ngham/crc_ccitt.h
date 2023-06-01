a0965ca28f64ebdc72c155aed665cd1c49e0126b
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2023-06-01 01:46:30 -0300

    Improving the structure

--- include/ngham/crc_ccitt.h
+++ include/ngham/crc_ccitt.h
@@ -1,9 +1,66 @@
-#ifndef CRC_CCITT_H
-#define	CRC_CCITT_H
+/*
+ * crc_ccitt.h
+ * 
+ * Copyright The NGHam Contributors.
+ * 
+ * This file is part of NGHam.
+ * 
+ * NGHam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NGHam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NGHam. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief CRC-CCITT definition.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.0.0
+ * 
+ * \date 2023/03/12
+ * 
+ * \defgroup crc-ccitt CRC-CCITT
+ * \ingroup ngham
+ * \{
+ */
+
+#ifndef CRC_CCITT_H_
+#define	CRC_CCITT_H_
 
 #include <stdint.h>
 
+/**
+ * \brief .
+ *
+ * \param[in] buf .
+ *
+ * \param[in] crc .
+ *
+ * \return .
+ */
 uint16_t crc_ccitt_byte(uint8_t buf, uint16_t crc);
+
+/**
+ * \brief .
+ *
+ * \param[in] buf .
+ *
+ * \param[in] buf_len .
+ *
+ * \return .
+ */
 uint16_t crc_ccitt(uint8_t *buf, uint16_t buf_len);
 
-#endif
+#endif /* CRC_CCITT_H_ */
+
+/**< \} End of crc-ccitt group */
--- include/ngham/ngham.h
+++ include/ngham/ngham.h
@@ -1,33 +1,98 @@
-//**************************************************************//
-// NGHam protocol - Jon Petter Skagmo, LA3JPA, 2014.            //
-// Licensed under LGPL.                                         //
-//**************************************************************//
+/*
+ * ngham.h
+ * 
+ * Copyright The NGHam Contributors.
+ * 
+ * This file is part of NGHam.
+ * 
+ * NGHam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NGHam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NGHam. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 
-#ifndef NGHAM_H
-#define NGHAM_H
+/**
+ * \brief NGHam library definition.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.0.0
+ * 
+ * \date 2023/03/12
+ * 
+ * \defgroup ngham NGHam
+ * \{
+ */
+
+#ifndef NGHAM_H_
+#define NGHAM_H_
 
 #include <stdint.h>
 
-#include "ngham_packets.h" // tx_pkt_t
+#include "ngham_packets.h"
+
+#define NGHAM_VERSION                   "v0.0.0"
 
 extern const uint8_t NGH_SYNC[];
 
-#define NGH_PREAMBLE_SIZE				4
-#define NGH_SYNC_SIZE					4
-#define NGH_PREAMBLE_SIZE_FOUR_LEVEL	8
-#define NGH_SYNC_SIZE_FOUR_LEVEL		8
-#define NGH_SIZE_TAG_SIZE				3
-#define NGH_MAX_CODEWORD_SIZE			255
-#define NGH_MAX_TOT_SIZE				(NGH_PREAMBLE_SIZE_FOUR_LEVEL+NGH_SYNC_SIZE_FOUR_LEVEL+NGH_SIZE_TAG_SIZE+NGH_MAX_CODEWORD_SIZE)
+#define NGH_PREAMBLE_SIZE               4U
+#define NGH_SYNC_SIZE                   4U
+#define NGH_PREAMBLE_SIZE_FOUR_LEVEL    8U
+#define NGH_SYNC_SIZE_FOUR_LEVEL        8U
+#define NGH_SIZE_TAG_SIZE               3U
+#define NGH_MAX_CODEWORD_SIZE           255
+#define NGH_MAX_TOT_SIZE                (NGH_PREAMBLE_SIZE_FOUR_LEVEL+NGH_SYNC_SIZE_FOUR_LEVEL+NGH_SIZE_TAG_SIZE+NGH_MAX_CODEWORD_SIZE)
 
-#define NGH_PADDING_bm                  0x1f
-#define NGH_FLAGS_bm                    0xe0
-#define NGH_FLAGS_bp                    5
+#define NGH_PADDING_bm                  0x1FU
+#define NGH_FLAGS_bm                    0xE0U
+#define NGH_FLAGS_bp                    5U
 
+/**
+ * \brief .
+ *
+ * \return None.
+ */
 void ngham_init_arrays(void);
+
+/**
+ * \brief .
+ *
+ * \return None.
+ */
 void ngham_deinit_arrays(void);
+
+/**
+ * \brief .
+ *
+ * \return None.
+ */
 void ngham_init(void);
-void ngham_encode(tx_pkt_t* p);
+
+/**
+ * \brief .
+ *
+ * \return None.
+ */
+void ngham_encode(tx_pkt_t *p);
+
+/**
+ * \brief .
+ *
+ * \param[in] d .
+ *
+ * \return None.
+ */
 void ngham_decode(uint8_t d);
 
-#endif
+#endif /* NGHAM_H_ */
+
+/**< \} End of ngham group */
