ff1165b987a4e1c33302c6792504e80b318ac6fa
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2023-06-16 00:34:47 -0300

    tests: Unit test of the initialization and encoding functions

--- tests/test.c
+++ tests/test.c
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.0.1
+ * \version 0.1.0
  * 
  * \date 2023/03/12
  * 
@@ -50,10 +50,34 @@ static void ngham_init_test(void **state)
 
 static void ngham_encode_test(void **state)
 {
+    uint8_t data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    uint16_t data_len = 10U;
+    uint8_t flags = 0U;
+
+    uint8_t pkt[300] = {0U};
+    uint16_t pkt_len = UINT16_MAX;
+
+    assert_return_code(ngham_encode(data, data_len, flags, pkt, &pkt_len), 0);
+
+    uint8_t expected_pkt[300] = {170, 170, 170, 170, 93, 230, 42, 126, 59, 73, 205, 237, 72, 15, 194, 153, 9, 117, 186, 137, 36, 154, 52, 142, 183, 70, 206, 90, 151, 125, 204, 50, 162, 191, 62, 10, 16, 241, 136, 148, 205, 234, 224, 253, 216, 143, 5, 252, 58, 82, 207, 117, 235, 194, 57, 208, 120, 236};
+    uint16_t expected_pkt_len = 58U;
+
+    assert_memory_equal((void*)pkt, (void*)expected_pkt, expected_pkt_len);
+    assert_int_equal(pkt_len, expected_pkt_len);
 }
 
 static void ngham_decode_test(void **state)
 {
+}
+
+static void crc_ccitt_byte_test(void **state)
+{
+//    assert_return_code(crc_ccitt_byte(), );
+}
+
+static void crc_ccitt_test(void **state)
+{
+//    assert_return_code(crc_ccitt(), );
 }
 
 int main()
@@ -62,6 +86,8 @@ int main()
         cmocka_unit_test(ngham_init_test),
         cmocka_unit_test(ngham_encode_test),
         cmocka_unit_test(ngham_decode_test),
+        cmocka_unit_test(crc_ccitt_byte_test),
+        cmocka_unit_test(crc_ccitt_test),
     };
 
     return cmocka_run_group_tests(ngham_tests, NULL, NULL);
